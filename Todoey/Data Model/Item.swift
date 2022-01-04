e28505e7057b9adba6b95f88393ff32978df27a6
Kirill Ki <kirillkalapov@gmail.com>
2022-01-04 18:29:20 +0100

    Refactor code

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by  Mr.Ki on 04.01.2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+    
+}
