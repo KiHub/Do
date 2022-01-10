3fadd6690d93e74efbbe4af41e2a0aa30afb6bd6
Kirill Ki <kirillkalapov@gmail.com>
2022-01-10 22:50:33 +0100

    Install and configure realm

---
+++ Todoey/Data Model/Data.swift
@@ -0,0 +1,16 @@
+//
+//  Data.swift
+//  Todoey
+//
+//  Created by  Mr.Ki on 10.01.2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+  @objc dynamic var name: String = ""
+   @objc dynamic var age: Int = 0
+    
+}
